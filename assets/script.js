f5d053832e2d52abcf73c007dd0e074093e14449
TJKlochowicz <t.j.klochowicz@uva.nl>
2023-02-16 12:30:00 +0100

    teaching section

--- assets/script.js
+++ assets/script.js
@@ -7,3 +7,24 @@ document.querySelectorAll('a[href^="#"]').forEach(anchor => {
         });
     });
 });
+
+const groupBy = function(xs, key) {
+    return xs.reduce(function(rv, x) {
+        (rv[x[key]] = rv[x[key]] || []).push(x);
+        return rv;
+    }, {});
+};
+
+engine.registerFilters({
+    group_by: (input, key) => {
+        const result = groupBy(input, key);
+        // map dictionary back to arry
+        return Object.keys(result).map(key1=>{
+            return {
+                key: key1,
+                items: result[key1]
+            };
+        });
+    }
+});
+
